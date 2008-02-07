695cbfcba6bb10b6adb059e56c2a7b0d9f892285
coneill <coneill@0ee00392-f00c-0410-a5b9-9db4dcba8d62>
2008-02-07 20:22:10 +0000

    Changed: Updated version numbers to 0.2.0 and added Changelog
    git-svn-id: https://www.eng.rr.com/svn/main/Config-Validate/trunk@2778 0ee00392-f00c-0410-a5b9-9db4dcba8d62

--- lib/Config/Validate.pm
+++ lib/Config/Validate.pm
@@ -21,7 +21,7 @@ use 5.008;
   our @EXPORT_OK = qw(validate mkpath);
   our %EXPORT_TAGS = ('all' => \@EXPORT_OK);
   
-  our $VERSION = '0.1.0';
+  our $VERSION = '0.2.0';
 
   my @schema              :Field 
                           :Accessor(schema) 
@@ -579,7 +579,7 @@ configuration files. (Or anywhere else)
 
 =head1 VERSION
 
-Version 0.1.0
+Version 0.2.0
 
 =head1 DESCRIPTION
 
